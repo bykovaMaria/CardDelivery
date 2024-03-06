9be3285e665f0c58515e62ed4326cc1e5b6a0d14
Maria test <bykova1985maria@gmail.com>
2024-03-06 22:00:05 +0700

    Changed gradle

---
+++ src/test/java/ru/netology/selenide/CardDeliveryFormTest.java
@@ -0,0 +1,33 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryFormTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    void shouldSendDeliveryForm() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Красноярск");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("ПЕТРОВ ПЕТР ПЕТРОВИЧ");
+        $("[data-test-id='phone'] input").setValue("+71111111111");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.ownText("Встреча успешно забронирована на " + planningDate));
+    }
+}
